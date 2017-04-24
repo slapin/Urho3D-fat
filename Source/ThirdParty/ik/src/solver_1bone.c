9c0aafab1e9df8519e0f8bd493e6b2e27d4ceeae
TheComet <alex.murray@gmx.ch>
2017-04-24 11:06:51 +0200

    1 bone solver

--- Source/ThirdParty/ik/src/solver_1bone.c
+++ Source/ThirdParty/ik/src/solver_1bone.c
@@ -1,6 +1,9 @@
-#include "ik/solver_1bone.h"
 #include "ik/chain.h"
+#include "ik/effector.h"
 #include "ik/log.h"
+#include "ik/node.h"
+#include "ik/solver_1bone.h"
+#include <assert.h>
 
 /* ------------------------------------------------------------------------- */
 int
@@ -31,7 +34,7 @@ solver_1bone_rebuild(ik_solver_t* solver)
      * sub chains.
      */
     ORDERED_VECTOR_FOR_EACH(&solver->chain_tree->children, ik_chain_t, child)
-        if (ordered_vector_count(&child->nodes) > 2) /* 2 nodes = 1 bone */
+        if (ordered_vector_count(&child->nodes) != 2) /* 2 nodes = 1 bone */
         {
             ik_log_message("WARNING: Your tree has chains that are longer than 1 bone. Are you sure you selected the correct solver algorithm?");
             return -1;
@@ -50,5 +53,24 @@ solver_1bone_rebuild(ik_solver_t* solver)
 int
 solver_1bone_solve(ik_solver_t* solver)
 {
+    ORDERED_VECTOR_FOR_EACH(&solver->chain_tree->children, ik_chain_t, child)
+        ik_node_t* node_tip;
+        ik_node_t* node_base;
+
+        node_tip = *(ik_node_t**)ordered_vector_get_element(&child->nodes, 0);
+        node_base = *(ik_node_t**)ordered_vector_get_element(&child->nodes, 1);
+
+        assert(node_tip->effector != NULL);
+        node_tip->position = node_tip->effector->target_position;
+
+        vec3_sub_vec3(node_tip->position.f, node_base->position.f);
+        vec3_normalise(node_tip->position.f);
+        vec3_mul_scalar(node_tip->position.f, node_tip->segment_length);
+        vec3_add_vec3(node_tip->position.f, node_base->position.f);
+    ORDERED_VECTOR_END_EACH
+
+    if (solver->flags & SOLVER_CALCULATE_FINAL_ROTATIONS)
+        calculate_global_rotations(solver->chain_tree);
+
     return 0;
 }
